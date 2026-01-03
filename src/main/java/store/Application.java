ff6b234b96dbb6277706d86f8116370713981470
2jaeheon <jaeheon0826@jbnu.ac.kr>
2026-01-03 10:33:48 +0900

    feat: 재수현을 위한 리셋
    Signed-off-by: 2jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,19 +1,7 @@
 package store;
 
-import store.controller.Controller;
-import store.domain.ProductLoader;
-import store.domain.PromotionVerifier;
-import store.view.InputView;
-import store.view.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        ProductLoader productLoader = new ProductLoader();
-        PromotionVerifier verifier = new PromotionVerifier();
-        Controller controller = new Controller(inputView, outputView, productLoader, verifier);
-
-        controller.run();
+        //TODO: Application 구현
     }
 }
